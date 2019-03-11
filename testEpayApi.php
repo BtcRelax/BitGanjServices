0e90bc841e674911f84999f45d576eaaffff31a5
godjah <godjah@bitganj.website>
2019-03-11 05:02:21 +0000

    Add property ProxyServer

--- testEpayApi.php
+++ testEpayApi.php
@@ -1,6 +1,10 @@
 <?php
 require 'EasyPayApi.php';
-$vApi = new \BtcRelax\EasyPayApi("380999411601","FirstPass$1");
+$vUser = $_GET["user"];
+$vPass = $_GET["pass"];
+$vProxy = $_GET["proxy"];
+$vApi = new \BtcRelax\EasyPayApi($vUser,$vPass);
+$vApi->setProxyServer($vProxy);
 $vGetSessionRes = $vApi->getSession();
 echo \sprintf("Result create session:%s\n<br>", $vGetSessionRes);
 if ($vGetSessionRes) {
